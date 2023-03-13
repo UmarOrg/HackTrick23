6a0142396ebab14726e8aa63fd5843f90bc0fd1f
Umar M.Ibrahim <umar.m.ibrahime@outlook.com>
2023-03-14 00:10:34 +0200

    test

--- gym-maze/_test_.py
+++ gym-maze/_test_.py
@@ -20,7 +20,9 @@ import tensorflow as tf
 import copy
 from tensorflow.python.client import device_lib 
 print(device_lib.list_local_devices())
-
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 
 import warnings
